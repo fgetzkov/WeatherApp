74bbc066bb10890e60fdc505a942dfe7fe0b33dc
philip <philip@linux-l5ln.suse>
2019-11-18 13:02:39 +0200

    Style the app

--- Forecast.js
+++ Forecast.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {StyleSheet, Text, View} from "react-native";
+
 class Forecast extends Component {
   render() {
     return (<View style={styles.container}>
