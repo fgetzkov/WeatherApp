a0bc653c8d0ad733bc5ba14d3af86fffec23b30a
philip <philip@linux-l5ln.suse>
2019-11-18 12:31:48 +0200

    Integrate with open weather API and display data

--- WeatherProject.js
+++ WeatherProject.js
@@ -1,19 +1,34 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, TextInput} from 'react-native';
+import React, {Component} from "react";
+import {StyleSheet, Text, View, TextInput} from "react-native";
+import Forecast from "./Forecast";
+import OpenWeatherMap from "./open_weather_map";
 
 class WeatherProject extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zip: ''
+      zip: "",
+      forecast: null
     };
   }
   _handleTextChange = event => {
-    this.setState({zip: event.nativeEvent.text});
+    let zip = event.nativeEvent.text;
+    OpenWeatherMap.fetchForecast(zip).then(forecast => {
+      console.log(forecast);
+      this.setState({forecast: forecast});
+    });
   };
+
   render() {
+    let content = null;
+    if (this.state.forecast !== null) {
+      content = (<Forecast main={this.state.forecast.main} description={this.state.forecast.description} temp={this.state.forecast.temp}/>);
+    }
     return (<View style={styles.container}>
-      <Text style={styles.welcome}>You input {this.state.zip}.</Text>
+      <Text style={styles.welcome}>
+        You input {this.state.zip}.
+      </Text>
+      {content}
       <TextInput style={styles.input} onSubmitEditing={this._handleTextChange}/>
     </View>);
   }
@@ -21,13 +36,13 @@ class WeatherProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#666666"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10
   },
   input: {
@@ -36,7 +51,7 @@ const styles = StyleSheet.create({
     padding: 2,
     height: 40,
     width: 100,
-    textAlign: 'center'
+    textAlign: "center"
   }
 });
 export default WeatherProject;
